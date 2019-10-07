6f8d8645eb1bcd848aa68b6927d4c30f11e2a9e1
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-07 18:39:16 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,16 @@
 # Use this space to code along with the readme
 $ greeeting.rb 
 
-
-
+def greeting
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+end 
